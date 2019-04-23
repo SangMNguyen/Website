42bcafcf9e973a71c0edf761979c059486c7f12a
Sang Nguyen <sangnguyenpublic@gmail.com>
2019-04-22 21:46:54 -0400

    Made Appearing Animation Smoother
    - Also added icons on the header bar for easier navigation

--- back.js
+++ back.js
@@ -3,24 +3,22 @@ window.addEventListener("scroll", function() {handleScroll()}, false);
 console.log("HELLO");
 function handleScroll() {
     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("topButton").style.display = "block";
+        document.getElementById("topButton").style.opacity = "1";
+        document.getElementById("topButton").style.transform = "scale(1)";
     } else {
-        document.getElementById("topButton").style.display = "none";
+        document.getElementById("topButton").style.opacity = "0";
+        document.getElementById("topButton").style.transform = "scale(0)";
+    }
+    if(document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+        document.getElementById("icons").style.opacity = "1";
+        document.getElementById("icons").style.transform = "scale(1)";
+    } else {
+        document.getElementById("icons").style.opacity = "0";
+        document.getElementById("icons").style.transform = "scale(0)";
     }
 }
 
 function toTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
-
-function goTo(section) {
-    if(section == "discord") {
-        document.body.scrollTop = 1000;
-        document.documentElement.scrollTop = 1000;
-    }
-    if(section == "pong") {
-        document.body.scrollTop = 2000;
-        document.documentElement.scrollTop = 2000;
-    }
 }
